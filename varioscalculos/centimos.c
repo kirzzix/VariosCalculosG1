fe618ea49301f088006f3bb2db20b6d4a4669919
juanmi <juanmi@juanmi-MS-7817>
2017-11-28 14:12:26 +0100

    funcion de centimos va todo

---
+++ varioscalculos/centimos.c
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: juanmi
+ *
+ * Created on 28 de noviembre de 2017, 12:48
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <math.h>
+
+#define FLT_ROUNDS 1;
+int centimos(float sueldo) {
+//int centimos (float sueldo){
+    float sueldoinicial;
+    double sueldoentero,sueldodecimales;
+    int mn050,mn020,mn010,mn005,mn002,mn001;
+    int resto;
+    //valor=sueldoinicial;
+    printf("Introduzca sueldo:\n");
+    scanf("%f",&sueldoinicial);
+    printf ("sueldoinicial= %0.2f \n",sueldoinicial);
+    //calcular el sueldo para los decimales;
+    sueldodecimales=(modf(sueldoinicial,&sueldoentero)*100.5);//modf vale para truncar el numero;esta incluido en math.h
+    //http://c.conclase.net/librerias/index.php?ansifun=modf#inicio;
+    
+    
+    
+    printf("su sueldo decimal es: \t %lf \n", sueldodecimales);
+    
+    
+    //calculo de los decimales concretos;
+    mn050=sueldodecimales/50;
+    printf("centimos de 50: \t %i \n ",mn050);
+    resto=sueldodecimales-(mn050*50);
+    mn020=resto/20;
+    printf("centimos de 20: \t %i \n ",mn020);
+    resto= resto -(mn020*20);
+    mn010=resto/10;
+    printf("centimos de 10: \t %i \n ",mn010);
+    resto= resto -(mn010*10);
+    mn005= resto/5;
+    printf("centimos de 5: \t %i \n ", mn005);
+    resto= resto-(mn005*5);
+    mn002= resto/2;
+    printf("centimos de 2: \t %i \n ", mn002);
+    resto=resto-(mn002*2);
+    mn001=resto;
+    printf("centimos de 1: \t  %i \n", mn001);
+
+
+
+
+
+
+    
+
+
+    return (mn050,mn020,mn010,mn005,mn002,mn001);
+}
