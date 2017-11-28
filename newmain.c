07c1bafe50464cf5627bfa386b584da4da41e1ac
paco <paco@paco-Aspire-7250>
2017-11-28 12:50:06 +0100

    Subida de funcion para validar numero

--- newmain.c
+++ newmain.c
@@ -17,8 +17,55 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
+int Calcular_Nif (int *y);  // funcion para calcular el Nif
+int Romanos (int *z);   //funcion para calcular en romanos
 
-    return (EXIT_SUCCESS);
+int Comprobacion(int *x ) 
+
+{
+    
+    
+    
+    
+    
+    if (*x==1) //cuando elegimos 1 en el menu comprueba que este entre los parametros 1 y 99999999
+                // si es asi, llamamos la funcion Dni
+    {
+        
+        if (numero>=1 && numero<=99999999) 
+          {
+              Calcular_Nif (&numero);
+          }
+          else
+          {
+              printf(" numero erroneo introduzca otra vez");
+              
+                           
+              
+    }
+    }
+    
+    
+    if (*x==2) //cuando elegimos 2 en el menu comprueba que este entre los parametros 1 y 3.999
+                // si es asi, llamamos la funcion romanos
+    { 
+        
+          if (numero>=1 && numero<=3999) 
+          {
+              Romanos (&numero);
+          }
+          else
+          {
+              printf("Numero para convertir a romano erroneo.\n Introduzca un numero entre el 0 y el 3999");
+    }
+    }
+    
+
+    
+    
+             
+
+    return (0);
 }
+
 
