43d2fa70fbf7ef19d653d08eef5d7ca643e2c0f8
esteban <istico92@gmail.com>
2017-11-30 10:31:48 +0100

    A esperas de que me suban los distintos ejercicios

--- main.c
+++ main.c
@@ -15,44 +15,61 @@
 //int comprobante(int z, char p[]);
 int main() {
     int numerico;
-    char numero1[20];
+    char numero1[20]; //Declaramos las variables como cadenas para poder después desglosar el número en dígitos
     char numero2[20];
-    int control;
+    int control; //Variable para indicar después que el bucle siga
   do
   {
-       system("CLEAR");
+       system("CLEAR"); //Limpiar pantalla cada vez que se ejecute el bucle
         printf("Seleccione la opción correspondiente a la aplicación que vaya a usar\n1. Obtención letra DNI\n2. Cálculo de números romanos\n");
-        printf("3.Calculo IMC\n4.Número de billetes\n0. Finalizar\n");
+        printf("3. Calculo IMC\n4. Número de billetes\n0. Finalizar\n");
         scanf("%i",&numerico);
-        if (numerico == 1 || numerico == 2)
-        {
-        printf("Indique el numero a transformar\n");
-        scanf("%s", &numero1);
-        }
         switch(numerico)
         {
             case 1 :
                 printf("Indique el numero de DNI\n");
                 scanf("%s", &numero1);
-                comprobante(numerico,numero1);
-                
+                /*if (comprobante(numerico,numero1))
+                {
+                    //operación del DNI
+                }*/
+                printf("Operación DNI\n");
                 break;
             case 2 :
                 printf("Indique el numero a transformar en romano\n");
                 scanf("%s", &numero1);
-                comprobante(numerico,numero);
+                /*if (comprobante(numerico,numero1))
+                {
+                    //operación de los romanos
+                }*/
+                printf("Operación Romanos\n");
                 break;
             case 3 :
                 printf("Indique el peso\n");
                 scanf("%s", &numero1);
-                printf("Indique la altura \n");
+                do 
+                {
+                printf("Indique la altura en metros usando el punto como separador decimal \n");
                 scanf("%s", &numero2);
-                comprobante(numerico,numero1,numero2);
+                if (atoi(numero2) >=2) 
+                {
+                    printf("Número erroneo introduzca de nuevo la altura\n");
+                }
+                }while (atoi(numero2) >= 3);
+                printf("Operación del IMC \n");
+                /*if (comprobante(numerico,numero1,numero2))
+                {
+                    //operación del IMC
+                }*/
                 break;
              case 4 :
-                printf("Indique el numero de DNI\n");
+                printf("Indique el sueldo\n");
                 scanf("%s", &numero1);
-                comprobante(numerico,numero);
+                /*if (comprobante(numerico,numero1))
+                {
+                    //operación de los billetes                
+                }*/
+                printf("Operación de los billetes \n");
                 break;
                                    
   
