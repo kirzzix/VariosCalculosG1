997cd3302f9fca980a60421a39ac16ac95a87a41
esteban <istico92@gmail.com>
2017-11-30 09:29:46 +0100

    Añadir opciones de menú y variables necesarias

--- main.c
+++ main.c
@@ -12,30 +12,50 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-int comprobante(int z, char p[]);
+//int comprobante(int z, char p[]);
 int main() {
     int numerico;
-    char numero[9];
+    char numero1[20];
+    char numero2[20];
     int control;
-    do
-   {
+  do
+  {
        system("CLEAR");
-        printf("Seleccione la opción correspondiente a la aplicación que vaya a usar\n1. Obtención letra DNI\n2. Cálculo de números romanos\n0. Finalizar\n");
+        printf("Seleccione la opción correspondiente a la aplicación que vaya a usar\n1. Obtención letra DNI\n2. Cálculo de números romanos\n");
+        printf("3.Calculo IMC\n4.Número de billetes\n0. Finalizar\n");
         scanf("%i",&numerico);
         if (numerico == 1 || numerico == 2)
         {
         printf("Indique el numero a transformar\n");
-        scanf("%s", &numero);
+        scanf("%s", &numero1);
         }
         switch(numerico)
         {
             case 1 :
-                comprobante(numerico,numero);
+                printf("Indique el numero de DNI\n");
+                scanf("%s", &numero1);
+                comprobante(numerico,numero1);
+                
                 break;
             case 2 :
+                printf("Indique el numero a transformar en romano\n");
+                scanf("%s", &numero1);
                 comprobante(numerico,numero);
                 break;
-            
+            case 3 :
+                printf("Indique el peso\n");
+                scanf("%s", &numero1);
+                printf("Indique la altura \n");
+                scanf("%s", &numero2);
+                comprobante(numerico,numero1,numero2);
+                break;
+             case 4 :
+                printf("Indique el numero de DNI\n");
+                scanf("%s", &numero1);
+                comprobante(numerico,numero);
+                break;
+                                   
+  
             case 0 :
                 printf("Programa finalizado");
                 break;
