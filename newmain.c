a556043b7c350a91e773c3c6e4c6d1be8ed408af
paco <paco@paco-Aspire-7250>
2017-11-30 13:00:38 +0100

    segunda prueba funcion validar

--- newmain.c
+++ newmain.c
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   newmain.c
- * Author: paco
- *
- * Created on 28 de noviembre de 2017, 12:40
- */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,112 +7,84 @@
 
 
 
-int Romanos(int *x);//Esta funcion manda a la funcion romanos el numero despues de hecha la comprobacion
-int Nif (int *y);//Esta funcion manda a la funcion Nif el numero despues de hecha la comprobacion
-int Billetes (int *b); //Esta funcion manda a la funcion Billetes el numero despues de hecha la comprobacion.
-int Imc (int *i); //Esta funcion manda a la funcion Imc el numero despues de hecha la comprobacion
-
-int Comprobacion ( int *z)//Se reciben los valores del menu de la main con un puntero
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+
+/*menu:   0.- Salir
+        1.- Dni
+        2.- Romano
+        3.- Imc
+        4.- Billetes*/
+        
+
+
+//int z variable z es la opcion del menu
+//char p[], char y[] 
+
+char main ()
 {
-    char numero[9]; //Recibimos el numero en esta variable?¿?
-    
-    int longitud;
-    int control = 0;
-    
-    
-    //printf("Esta es \n");
-    scanf("%s", &numero);
-    longitud = strlen(numero);      //longitud toma el valor del numero de caracteres
-    printf( "strlen(numero) = %d\n", strlen(numero) );
 
-    printf("Esta es la longitud %i\n",longitud);
-   
-for (int a=0;a <= longitud ;a++) //Comprobamos uno a uno si son numeros
-    {
-        if (isdigit(numero[a]))
-        {
-            printf("Este es el valor que está cogiendo %c\n",numero[a]);
-            control++;
-            if (a == longitud)
+        char cadena[10];
+        int error=0;
+        
+        printf("Introduzca caracter\n");
+        scanf("%s", cadena);
+
+        printf("el caracter es: %s", cadena);
+        int longitud = strlen(cadena);
+        printf("\nsu longitud es de:%i\n", longitud);
+        
+        
+       //if (menu == 1) || (menu == 2)    //opcion para cuando eligen Dni o Romano 
+        {          
+            for (int b = 0 ;b < longitud ;b++) //Comprobamos uno a uno si son numeros
             {
+                if ((isdigit(cadena[b])))
+                {
+                    printf("Este es el valor que está cogiendo %c\n",cadena[b]);
+                }
+                else
+                {
+                    printf("\nNumero erroneo. \nIntroduzca de nuevo el número\n");
                 break;
+                }  
             }
-        }else{
-                printf("Numero erroneo. \n Introduzca de nuevo el número\n");
-            break;
         }
-      
-  
-           
-    }
-if (control == longitud)
-    {
-        int numeras = atoi(numero);
-        printf("Este es el numero %i\n",numeras);
-        if (*z == 1)
-        {
-            if (( numeras > 0) && ( numeras <= 99999999))
+        
+        
+        //if ((menu == 3) || (menu == 4))//opcion para cuando se eligen IMC o Billetes
+        {          
+            for (int b = 0 ;b < longitud ;b++) //Comprobamos uno a uno si son numeros
             {
-               Nif(&numeras); //nif(numeras);
-            }
-            else{
-                printf("Introduzca un valor comprendido entre 00000001 y 99999999");
-            }
-        }
-        if (*z == 2)
-        {
-            if ((( numeras > 0) && ( numeras <= 3999))  )
-            {
-                Romanos(&numeras);
-            }
-            else{
-                printf("Introduzca un número entero válido comprendido entre 1 y 3999");
-            }
-        }
-        if (*z == 3)
-        {
-            if ((( numeras > 0) && ( numeras <= 3999))  )
-            {
-                Romanos(&numeras);
-            }
-            else{
-                printf("Introduzca un número entero válido comprendido entre 1 y 3999");
+                if ((isdigit(cadena[b]) || (cadena[b]=='.')))
+                {
+                    printf("Este es el valor que está cogiendo %c\n",cadena[b]);
+                    if ((cadena[b]=='.')) //añadimos una excepcion para el acepte el '.', pero no el segundo '.'
+                    {
+                        if (error <= 0)  
+                        {
+                            error++;
+                        }
+                        else
+                        {
+                             //printf(".excesivo");
+                        break; 
+                        }  
+                    }
+                }
+                else
+                {
+                    printf("\nNumero erroneo. \nIntroduzca de nuevo el número\n");
+                break;
+                }  
             }
         }
-        
-         if (*z == 4)
-        {
-            if ((( numeras > 0) && ( numeras <= 3999))  )
-            {
-                Romanos(&numeras);
-            }
-            else{
-                printf("Introduzca un número entero válido comprendido entre 1 y 3999");
-            }
-        
-        
-        
-      
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+ //return("true");       
 }
- 
-    return(0);
-}
+    
